7c45ea811404a5044b7658d184a2df5bdb64594f
frknsagg <furkansag_21@hotmail.com>
2023-12-22 16:40:37 +0300

    entities update

--- src/main/java/com/example/rentacarv1/repositories/BrandRepository.java
+++ src/main/java/com/example/rentacarv1/repositories/BrandRepository.java
@@ -1,6 +1,6 @@
 package com.example.rentacarv1.repositories;
 
-import com.example.rentacarv1.Entities.Brand;
+import com.example.rentacarv1.entities.Brand;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BrandRepository extends JpaRepository<Brand,Integer> {

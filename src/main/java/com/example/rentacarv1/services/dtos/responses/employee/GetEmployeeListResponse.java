8050f8399b16deb44ab66d7093249d99de3a72a8
frknsagg <furkansag_21@hotmail.com>
2024-01-26 22:59:59 +0300

    added serializable for cache

--- src/main/java/com/example/rentacarv1/services/dtos/responses/employee/GetEmployeeListResponse.java
+++ src/main/java/com/example/rentacarv1/services/dtos/responses/employee/GetEmployeeListResponse.java
@@ -4,10 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class GetEmployeeListResponse {
+public class GetEmployeeListResponse  implements Serializable {
     private  int id;
     private double salary;
 }

2fb886218d721766a8308323a6e987113e2bda50
Aslı Beyhan <77741685+aslibeyhan@users.noreply.github.com>
2024-02-02 15:50:49 +0300

    logsController update

--- src/main/java/com/example/rentacarv1/controllers/AuthController.java
+++ src/main/java/com/example/rentacarv1/controllers/AuthController.java
@@ -1,6 +1,4 @@
 package com.example.rentacarv1.controllers;
-
-import com.example.rentacarv1.core.services.JwtService;
 import com.example.rentacarv1.services.abstracts.AuthService;
 import com.example.rentacarv1.services.dtos.requests.auth.LoginRequest;
 import com.example.rentacarv1.services.dtos.requests.auth.RegisterRequest;
@@ -8,14 +6,10 @@ import com.example.rentacarv1.services.dtos.responses.auth.AuthenticationRespons
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+
 
 @RestController
 @RequestMapping("/api/auth")

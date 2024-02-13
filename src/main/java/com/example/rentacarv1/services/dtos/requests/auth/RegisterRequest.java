a89b230f628576dbade609435c2c609a917449e2
Byulutas <ulutasbayram7@gmail.com>
2024-02-13 22:34:37 +0300

    Validation added.

--- src/main/java/com/example/rentacarv1/services/dtos/requests/auth/RegisterRequest.java
+++ src/main/java/com/example/rentacarv1/services/dtos/requests/auth/RegisterRequest.java
@@ -1,6 +1,10 @@
 package com.example.rentacarv1.services.dtos.requests.auth;
 
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,10 +16,26 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class RegisterRequest {
 
+    @NotBlank(message = "Email cannot be blank")
+    @Email(message = "Invalid email address format")
     private String email;
 
+    @NotBlank
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=.*])(?=\\S+$).{8,}",
+            message = "At least 8 characters\n" +
+                    "\n" +
+                    "Contains at least one digit\n" +
+                    "\n" +
+                    "Contains at least one lowercase and one uppercase letter\n" +
+                    "\n" +
+                    "Contains at least one special character from the set (@#%$^.*etc.)\n" +
+                    "\n" +
+                    "Does not contain spaces, tabs, etc.")
     private String password;
 
+
+    @NotBlank(message = "Name cannot be blank")
+    @Size(min = 2, max = 20, message = "Role name must be between 2 and 20 characters")
     private String roleName;
 
 }
